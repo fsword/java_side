b2027252fc533ae173e783fff1bfce217ad26b6f
fsword <li.jianye@gmail.com>
2011-05-27 01:43:47 +0800

    change

--- install.rb
+++ install.rb
@@ -4,6 +4,9 @@ require 'fileutils'
 rails_root=File.expand_path __FILE__+"/../../../"
 asset=File.expand_path __FILE__+'/install_assets/'
 
+p rails_root
+p asset
+
 Dir.mkdir rails_root+"config/java" unless File.exist? rails_root+"config/java"
 File.copy(asset+'springbeans.xml', rails_root+'config/java',true) unless File.exist? rails_root+'config/java/springbeans.xml'
 
