093f4cd222c5fa7689863665f2cf1bea49277a82
fsword <li.jianye@gmail.com>
2011-05-27 02:18:50 +0800

    test

--- install.rb
+++ install.rb
@@ -3,21 +3,12 @@ require 'ftools'
 rails_root=File.expand_path __FILE__+"/../../../.."
 asset=File.expand_path __FILE__+'/../install_assets'
 
-p rails_root
-p asset
-
 Dir.mkdir rails_root+"/config/java" unless File.exist? rails_root+"/config/java"
-File.copy(asset+'/springbeans.xml', rails_root+'/config/java',true) unless File.exist? rails_root+'/config/java/springbeans.xml'
-
-p 'x'
-
 Dir.mkdir rails_root+"/maven_jars" unless File.exist? rails_root+"/maven_jars"
 
-p 'a'
+File.copy(asset+'/springbeans.xml', rails_root+'/config/java',true) unless File.exist? rails_root+'/config/java/springbeans.xml'
 
 File.copy(asset+'/pom.xml', rails_root+'/maven_jars') unless File.exist? rails_root+'/maven_jars/pom.xml'
 
-p 'b'
-
-File.copy(asset+file, rails_root+'/script',true) unless File.exist? rails_root+'/script/start.sh'
+File.copy(asset+'/start.sh', rails_root+'/script',true) unless File.exist? rails_root+'/script/start.sh'
 
