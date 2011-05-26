04dd6d2fc4565b05790b57844cde91a33ac3b4ac
fsword <li.jianye@gmail.com>
2011-05-27 01:53:49 +0800

    fix

--- install.rb
+++ install.rb
@@ -7,13 +7,13 @@ asset=File.expand_path __FILE__+'/../install_assets/'
 p rails_root
 p asset
 
-Dir.mkdir rails_root+"config/java" unless File.exist? rails_root+"config/java"
-File.copy(asset+'springbeans.xml', rails_root+'config/java',true) unless File.exist? rails_root+'config/java/springbeans.xml'
+Dir.mkdir rails_root+"/config/java" unless File.exist? rails_root+"/config/java"
+File.copy(asset+'/springbeans.xml', rails_root+'/config/java',true) unless File.exist? rails_root+'/config/java/springbeans.xml'
 
-FileUtils.mkdir_p rails_root+"maven_jars/src/main/java"
-FileUtils.mkdir_p rails_root+"maven_jars/src/test/java"
+FileUtils.mkdir_p rails_root+"/maven_jars/src/main/java"
+FileUtils.mkdir_p rails_root+"/maven_jars/src/test/java"
 
-File.copy(asset+'pom.xml', rails_root+'maven_jars') unless File.exist? rails_root+'maven_jars/pom.xml'
+File.copy(asset+'/pom.xml', rails_root+'/maven_jars') unless File.exist? rails_root+'/maven_jars/pom.xml'
 
-File.copy(asset+file, rails_root+'script',true) unless File.exist? rails_root+'script/start.sh'
+File.copy(asset+file, rails_root+'/script',true) unless File.exist? rails_root+'/script/start.sh'
 
--- uninstall.rb
+++ uninstall.rb
@@ -3,8 +3,8 @@ require 'fileutils'
 
 rails_root=File.expand_path __FILE__+"/../../../"
 
-FileUtils.rm_rf rails_root+'config/java'
+FileUtils.rm_rf rails_root+'/config/java'
 
-FileUtils.rm_rf rails_root+'maven_jars'
+FileUtils.rm_rf rails_root+'/maven_jars'
 
-FileUtils.rm_rf rails_root+'script/start.sh'
+FileUtils.rm_rf rails_root+'/script/start.sh'
