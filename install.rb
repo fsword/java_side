f9a8128b63896d48a1529ba421a86c0eeeb310bf
fsword <li.jianye@gmail.com>
2011-05-27 02:03:45 +0800

    fix

--- install.rb
+++ install.rb
@@ -10,8 +10,7 @@ p asset
 Dir.mkdir rails_root+"/config/java" unless File.exist? rails_root+"/config/java"
 File.copy(asset+'/springbeans.xml', rails_root+'/config/java',true) unless File.exist? rails_root+'/config/java/springbeans.xml'
 
-FileUtils.mkdir_p rails_root+"/maven_jars/src/main/java"
-FileUtils.mkdir_p rails_root+"/maven_jars/src/test/java"
+FileUtils.mkdir_p rails_root+"/maven_jars"
 
 File.copy(asset+'/pom.xml', rails_root+'/maven_jars') unless File.exist? rails_root+'/maven_jars/pom.xml'
 
