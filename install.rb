e7fff3e61ad29e52f789a8f26cbaea62bd927d6a
fsword <li.jianye@gmail.com>
2011-05-27 02:23:58 +0800

    fix

--- install.rb
+++ install.rb
@@ -1,10 +1,12 @@
 require 'ftools'
+require 'fileutils'
 
 rails_root=File.expand_path __FILE__+"/../../../.."
 asset=File.expand_path __FILE__+'/../install_assets'
 
 Dir.mkdir rails_root+"/config/java" unless File.exist? rails_root+"/config/java"
-Dir.mkdir rails_root+"/maven_jars" unless File.exist? rails_root+"/maven_jars"
+FileUtils.mkdir_p rails_root+"/maven_jars/src/test/java" unless File.exist? rails_root+"/maven_jars/src/test/java"
+FileUtils.mkdir_p rails_root+"/maven_jars/src/main/java" unless File.exist? rails_root+"/maven_jars/src/main/java"
 
 File.copy(asset+'/springbeans.xml', rails_root+'/config/java',true) unless File.exist? rails_root+'/config/java/springbeans.xml'
 
