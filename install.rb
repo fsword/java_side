d2d7dcb527af42d8f618a77901674dbe7e2454a5
fsword <li.jianye@gmail.com>
2011-05-27 01:40:44 +0800

    fix it

--- install.rb
+++ install.rb
@@ -1,8 +1,8 @@
 require 'ftools'
 require 'fileutils'
 
-rails_root=File.dirname(__FILE__)+"/../../../"
-asset=File.dirname(__FILE__)+'/install_assets/'
+rails_root=File.expand_path __FILE__+"/../../../"
+asset=File.expand_path __FILE__+'/install_assets/'
 
 Dir.mkdir rails_root+"config/java" unless File.exist? rails_root+"config/java"
 File.copy(asset+'springbeans.xml', rails_root+'config/java',true) unless File.exist? rails_root+'config/java/springbeans.xml'
