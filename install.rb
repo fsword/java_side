c6ff71912252b76b68066080306eecddb41db8a8
fsword <li.jianye@gmail.com>
2011-05-27 01:07:15 +0800

    fix it

--- install.rb
+++ install.rb
@@ -6,14 +6,10 @@ asset=File.dirname(__FILE__)+'/install_assets/'
 Dir.mkdir rails_root+"config/java" unless File.exist? rails_root+"config/java"
 File.copy(asset+'springbeans.xml', rails_root+'config/java',true) unless File.exist? rails_root+'config/java/springbeans.xml'
 
-Dir.mkdir rails_root+"test/java" unless File.exist? rails_root+"test/java"
-File.copy(asset+'springbeans.xml', rails_root+'test/java',true) unless File.exist? rails_root+'test/java/springbeans.xml'
-
 Dir.mkdir rails_root+"maven_jars/src/main/java"
 Dir.mkdir rails_root+"maven_jars/src/test/java"
 
 File.copy(asset+'pom.xml', rails_root+'maven_jars') unless File.exist? rails_root+'maven_jars/pom.xml'
 
-%w{jruby-console jruby-server start.sh}.each{|file|
-  File.copy(asset+file, rails_root+'script',true) unless File.exist? rails_root+'script/'+file
-}
+File.copy(asset+file, rails_root+'script',true) unless File.exist? rails_root+'script/start.sh'
+
--- uninstall.rb
+++ uninstall.rb
@@ -3,3 +3,7 @@ require 'ftools'
 rails_root=File.dirname(__FILE__)+"/../../../"
 
 FileUtils.rm_rf rails_root+'config/java'
+
+FileUtils.rm_rf rails_root+'maven_jars'
+
+FileUtils.rm_rf rails_root+'script/start.sh'
