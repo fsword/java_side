b3fd9276f9be4896f78a55411b3ab66409d52a52
fsword <li.jianye@gmail.com>
2011-05-27 01:33:28 +0800

    fix it

--- install.rb
+++ install.rb
@@ -6,8 +6,8 @@ asset=File.dirname(__FILE__)+'/install_assets/'
 Dir.mkdir rails_root+"config/java" unless File.exist? rails_root+"config/java"
 File.copy(asset+'springbeans.xml', rails_root+'config/java',true) unless File.exist? rails_root+'config/java/springbeans.xml'
 
-Dir.mkdir rails_root+"maven_jars/src/main/java"
-Dir.mkdir rails_root+"maven_jars/src/test/java"
+FileUtils.mkdir_p rails_root+"maven_jars/src/main/java"
+FileUtils.mkdir_p rails_root+"maven_jars/src/test/java"
 
 File.copy(asset+'pom.xml', rails_root+'maven_jars') unless File.exist? rails_root+'maven_jars/pom.xml'
 
