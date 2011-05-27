695d2cf653df71b1f6158fd9e8a8ee467cba68a2
fsword <li.jianye@gmail.com>
2011-05-27 19:17:22 +0800

    fix

--- init.rb
+++ init.rb
@@ -3,7 +3,10 @@ if RUBY_PLATFORM =~ /java/
     require jar_file
     p "load jar: #{jar_file}"
   } if File.exist? Rails.root + "java/jars"
-  $CLASSPATH << "java/conf/" if File.exist? Rails.root + "java/conf/"
+  if File.exist? Rails.root + "java/conf/"
+    $CLASSPATH << "java/conf/"
+    p "add classpath: java/conf/"
+  end
   begin
     require 'java_side'
   rescue Exception => e
