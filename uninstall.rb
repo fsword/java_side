2b0d91b71b7d0e89fc77ff9155acff122edc543c
fsword <li.jianye@gmail.com>
2011-05-27 13:08:39 +0800

    remove jars

--- uninstall.rb
+++ uninstall.rb
@@ -5,6 +5,8 @@ rails_root=File.expand_path __FILE__+"/../../../.."
 
 FileUtils.rm_rf rails_root+'/config/java'
 
+FileUtils.rm_rf rails_root+'/jars'
+
 FileUtils.rm_rf rails_root+'/maven'
 
 FileUtils.rm_rf rails_root+'/script/start.sh'
